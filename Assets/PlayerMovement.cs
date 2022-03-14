7abf9c657454c4716e925adf7f4f45e86a9a81dd
bhixsonsimeral <bhixson-simeral@healthx.com>
2022-03-14 00:26:02 -0400

    Tutorial 4. Movement. Added movement to player and keychecks for left and right.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,6 +4,9 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public Rigidbody rigidbody;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,9 +14,26 @@ public class PlayerMovement : MonoBehaviour
 
     // Update is called once per frame
     // We use FixedUpdate as it is prefered when dealing with physics
+
+    // When checking for player input, we should instead do this in Update()
+    // FixedUpdate has the possibilty of running slower and missing an input
+    // This would normally be handled by checking for player input and storing
+    // it in a variable of some sort (potentially the boolean moveRight)
     void FixedUpdate()
     {
         // Time.deltaTime is the amount of time since the last frame
-        rigidbody.AddForce(0, 0, 2000 * Time.deltaTime);
+        // Add a forward force
+        rigidbody.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        // Input.GetKey is limited as it will not work with a controller
+        // This implementation also does not allow for substitute keys
+        if (Input.GetKey("d") )
+        {
+            rigidbody.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rigidbody.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
