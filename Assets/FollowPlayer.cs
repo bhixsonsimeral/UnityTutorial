43da6f683987077730a6e13bd0adfbc1aa431bf3
bhixsonsimeral <bhixson-simeral@healthx.com>
2022-03-14 03:00:26 -0400

    Tutorial 4, camera follow. Played around with adding zoom

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 cameraOffset;
+    public float scale;
+    // Update is called once per frame
+    void Update()
+    {
+        // This doesn't do exactly what I want, as it zooms along an axis
+        // instead of zooming into the player. I would bet there is a built
+        // in fuction for doing what I want.
+        if (Input.mouseScrollDelta != new Vector2(0, 0))
+        {
+            // Vector3.forward is a unit vector for the way the object is facing
+            // The camera is not facing the player, so this also does not achieve
+            // out desired result.
+            cameraOffset += Vector3.forward * Input.mouseScrollDelta.y * scale;
+        }
+        transform.position = player.position + cameraOffset;
+    }
+}
