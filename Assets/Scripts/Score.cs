81572301551c9dac5c3f4d3a42b384ae62fdbf7c
bhixsonsimeral <bhixson-simeral@healthx.com>
2022-03-14 22:48:48 -0400

    Tutorial 7, score and ui. Created and referenced text object in game. Tied it to player postion

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,12 @@
+using TMPro;
+using UnityEngine;
+public class Score : MonoBehaviour
+{
+    public Transform player;
+    public TextMeshProUGUI scoreText;
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = player.position.z.ToString("0");
+    }
+}
